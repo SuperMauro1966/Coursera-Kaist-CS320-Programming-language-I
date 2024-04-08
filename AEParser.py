1e88c48af466e44d1e6766b9941010094ce72fb6
Mauro <msgarzi@imolainformatica.it>
2024-04-08 14:46:55 +0200

    AEParser - WIP

--- AEParser.py
+++ AEParser.py
@@ -1,7 +1,10 @@
 import re
 import logging
+import string
+
 import AE as lang
 
+logging.basicConfig()
 logger = logging.getLogger("AEParser")
 logger.setLevel(logging.DEBUG)
 
@@ -13,13 +16,11 @@ e ::= n
 """
 
 # pattern
-DIGIT = r"\d+"
+DIGIT = r"(\d+)"
 WHITESPACES = r"\s*"
 
 OPENPAR = r"\("
 CLOSEDPAR = r"\)"
-
-ANYOTHER = r"(.*)"
 
 OPPLUS = r"\+"
 OPSUB = r"\-"
@@ -28,116 +29,127 @@ OPSUB = r"\-"
 class ParserException(Exception):
     pass
 
-class ADDOPException(ParserException):
-    pass
-
-class SUBOPException(ParserException):
-    pass
-
 class NumException(ParserException):
     pass
 
-ADDEXPR = WHITESPACES + OPENPAR + WHITESPACES  \
-        + ANYOTHER + WHITESPACES + OPPLUS + WHITESPACES + ANYOTHER \
-        + WHITESPACES +CLOSEDPAR + WHITESPACES
+class SymbolException(ParserException):
+    pass
 
-SUBEXPR = WHITESPACES + OPENPAR + WHITESPACES  \
-        + ANYOTHER + WHITESPACES + OPSUB + WHITESPACES + ANYOTHER \
-        + WHITESPACES +CLOSEDPAR + WHITESPACES
+def ConsumeSymbol(sym: str, prog: str)-> str:
+    logger.debug(f"ConsumeSymbol called - {sym=!s} {prog=!s}")
+    prog = prog.lstrip(string.whitespace)
+    m = re.match(sym, prog)
 
-NUMBER = WHITESPACES + r"(\d+)" + WHITESPACES
+    logger.debug(f"Match: {m!s}")
+    if m is None:
+        raise SymbolException(f"Symbol {sym!s} not found")
 
-def ADDParser(s: str, pexpr: str):
-    p = re.compile(pexpr) 
-    m = p.match(s)
+    return prog[m.end():]
 
-    logger.debug(f"ADDParser {m}")
+def SpitAtSymbol(sym: str, prog: str)-> tuple[str, str]:
+    prog = prog.lstrip(string.whitespace)
+    m = re.search(sym, prog)
+
+    if m is None:
+        raise SymbolException(f"Symbol {sym!s} not found")
     
-    if m is not None:
-        leftop = m.group(1)
-        rightop = m.group(2)
+    return (prog[:m.start()], prog[m.end():])
 
-        logger.debug(f"ADDParser {leftop=} {rightop=}") 
-        
-        leftop = Parse(leftop, EXPR_START)
-        rightop = Parse(rightop, EXPR_START)
-        return lang.Add(leftop, rightop)
-    
-    raise ADDOPException(f"Not an ADD expression: {s}")
+def NumberId(prog:str, num_pattern: str)-> tuple[str, str]:
+    logger.debug(f"NumberId called: {prog=} {num_pattern=}")
+    prog = prog.lstrip(string.whitespace)
+    m = re.match(num_pattern, prog)
 
-def SUBParser(s: str, pexpr: str):
-    p = re.compile(pexpr) 
-    m = p.match(s)
+    logger.debug(f"Match {m=!s}")
 
-    logger.debug(f"SUBParser {m}")
-    
-    if m is not None:
-        leftop = m.group(1)
-        rightop = m.group(2)
-
-        logger.debug(f"SUBParser {leftop=} {rightop=}")
-        
-        leftop = Parse(leftop, EXPR_START)
-        rightop = Parse(rightop, EXPR_START)
-        return lang.Sub(leftop,rightop)
-    
-    raise SUBOPException(f"Not and ADD expression: {s}")
-
-def NumberId(s:str, pexpr: str):
-    p = re.compile(pexpr) 
-    m = p.match(s)
-
-    logger.debug(f"NumberId {m}")
-    
     if m is not None:
         num = m.group(1)
-        logging.debug(f"NumberId {num=}")
-        return lang.Num(int(num))
+        logger.debug(f"NumberId {num=}")
+        return num, prog[m.end():]
     
-    raise NumException(f"Not and number: {s}")
+    raise NumException(f"Not and number: {prog}")
 
-EXPR_START = { 
-    NUMBER: NumberId,
-    ADDEXPR: ADDParser,
-    SUBEXPR: SUBParser}
+def iterParser(s)->tuple[lang.Expression, str]:
+    logger.debug(f"iterparser - {s}")
+    if len(s) == 0:
+        return None, ""
+    
+    gen_expr = None
+    try:
+        s = ConsumeSymbol(OPENPAR, s)
+    except SymbolException:
+        # Num + Num
+        # Num - Num
+        # possible patterns
 
-
-def Parse(s, root= EXPR_START)->lang.Expression:
-    prog = s.replace('\n', ' ').replace('\r',' ').replace('\r\n',' ')
-
-    for pattern, parsePattern in root.items():
         try:
-            logger.debug(f"calling {parsePattern.__name__} with {pattern}")
-            return parsePattern(prog, pattern)
-        except ParserException as e:
-            logger.debug("generated exception")
-            logger.debug(f"Source string: {s}")
-            logger.debug(e)
+            left, s = SpitAtSymbol(OPPLUS, s)
+        except SymbolException:
+            try:
+                left, s = SpitAtSymbol(OPSUB, s)
+            except SymbolException:
+                raise
+            else:
+                plus_operator = False
+        else:
+            plus_operator = True
 
-    raise ParserException(f"Expression not recognized: {s}")
+        num, right_part = NumberId(left, DIGIT)
+        left_op = lang.Num(int(num))
+        checkEmptyTailProgram(right_part)
 
+        right_op, s = Parser(s) 
+        
+        # checkEmptyTailProgram(right_part)
+
+        if plus_operator:
+            return lang.Add(left_op, right_op), s
+        
+        return lang.Sub(left_op, right_op), s
+
+    else:
+        gen_expr, s = Parser(s)
+        s = ConsumeSymbol(CLOSEDPAR, str)
+        return gen_expr, s
+    
+def checkEmptyTailProgram(prog: str):
+    logger.debug(f"checkEmptyTailProgram called - {prog=!s}")
+    prog = prog.lstrip(string.whitespace).rstrip(string.whitespace)
+    if len(prog) != 0:
+        raise ParserException(f"Error: Found {prog} instead of space chars")
+
+def Parser(s)->lang.Expression:
+    logger.debug(f"Parser - {s=}")
+    if len(s) == 0:
+        return None
+    
+    gen_expr = None
+    try:
+        s = ConsumeSymbol(OPENPAR, s)
+    except SymbolException:
+        # Num expected
+        num, s = NumberId(s, DIGIT)
+        gen_expr = lang.Num(int(num))
+        # checkEmptyTailProgram(s)
+    else:
+        gen_expr , s = iterParser(s) 
+        s = ConsumeSymbol(CLOSEDPAR, s)
+
+    return gen_expr, s
 
 if __name__ == "__main__":
-    expr_list = ["  ( Ciccio + Caio      )",
-             "  ( Ciccio + Caio      ",
-             "  ( 10  + 20)",
-             "  (10 + 20",
-             "(10 + (20 - 1))",
-             "(20 - 1)",
-             "  10",
-             """
-             (
-             10
-                +
-            20
-             )
-             """]
+    expr_list = [
+             "10",
+             " ( 10 + 20        )",
+             " ( 10 + (20 + 30 ))",
+             " (( 10 +     1) + ( 3 - 1) )",
+             ]
 
 
     for el in expr_list:
         try:
             print(f"parsing {el}")
-            code = Parse(el, EXPR_START)
+            code, _ = Parser(el)
             print(f"{code=!s}")
             res = lang.interp(code)
             print(f"result of execution {res}")
